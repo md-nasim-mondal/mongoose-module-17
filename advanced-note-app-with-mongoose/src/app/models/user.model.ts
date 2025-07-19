3fa62a5be07388c2151c725e03ecf0a66d9b997d
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-07-19 21:21:23 +0600

    complete module 18 - 2 and learn about validate, custom validator in schema

--- advanced-note-app-with-mongoose/src/app/models/user.model.ts
+++ advanced-note-app-with-mongoose/src/app/models/user.model.ts
@@ -1,16 +1,49 @@
 import { model, Schema } from "mongoose";
-import type { IUser } from "../interfaces/user.inteface";
+import type { IUser } from "../interfaces/user.interface";
+import validator from "validator"
 
 const userSchema = new Schema<IUser>({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, trim: true },
-  password: {type: String, required: true},
+  firstName: {
+    type: String,
+    required: [true, "firstName is required!!"],
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  lastName: { type: String, required: true, trim: true },
+  age: {
+    type: Number,
+    required: true,
+    // min: [18, "Age Must be at least 18, got {VALUE}"],
+    min: 18,
+    max: 60,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: [true, "This email already used!!"],
+    trim: true,
+    // validate: {
+    //   validator: function (v) {
+    //     return /^[^\s@]+\.[^\s@]+$/.test(v);
+    //   },
+    //   message: function(props) {
+    //     return `Email ${props?.value} is not valid email!`
+    //   }
+    // },
+    validate:[validator.isEmail, "Invalid Email Sent {VALUE}!"]
+  },
+  password: { type: String, required: true, trim: true },
   role: {
     type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  }
+    // enum: ["user", "admin"],
+    enum: {
+      values: ["user", "admin"],
+      message: "Role is not valid! got {VALUE} role",
+    },
+    default: "user",
+  },
 });
 
-export const User = model("User", userSchema)
+export const User = model("User", userSchema);
